9ec9f534f312d8aafd714f92c44d17d04969dee0
huimintoh <hmintoh@gmail.com>
2018-12-06 16:35:52 +0800

    refactor updateQuality() to extract name into functions

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,24 +10,31 @@ class Shop {
   constructor(items = []) {
     this.items = items;
   }
+
+  isAgedBrie(i) {
+    return this.items[i].name === "Aged Brie";
+  }
+
+  isBackstagePass(i) {
+    return this.items[i].name === "Backstage passes to a TAFKAL80ETC concert";
+  }
+
+  isSulfuras(i) {
+    return this.items[i].name === "Sulfuras, Hand of Ragnaros";
+  }
+
   updateQuality() {
-    //loop though the
     for (let i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
+      if (!this.isAgedBrie(i) && !this.isBackstagePass(i)) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+          if (!this.isSulfuras(i)) {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+          if (this.isBackstagePass(i)) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -41,16 +48,14 @@ class Shop {
           }
         }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+      if (!this.isSulfuras(i)) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+        if (!this.isAgedBrie(i)) {
+          if (!this.isBackstagePass(i)) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+              if (!this.isSulfuras(i)) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
